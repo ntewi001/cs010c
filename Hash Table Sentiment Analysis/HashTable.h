93a8a9d7433c4849143a4d7f868854916470663c
ntewi001 <137817477+ntewi001@users.noreply.github.com>
2023-06-27 00:29:44 +0700

    Add files via upload

---
+++ Hash Table Sentiment Analysis/HashTable.h
@@ -0,0 +1,31 @@
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include <string>
+#include <list>
+#include <iostream>
+#include "WordEntry.h"
+
+using namespace std;
+
+class HashTable {
+
+ private:
+	list<WordEntry> *hashTable;
+	int size;
+
+ public:
+	~HashTable();
+	HashTable(int);
+	bool contains(const string &);
+	double getAverage(const string &);
+	void put(const string &, int);
+ 
+ private:
+	int computeHash(const string &);
+	// rule of 3
+	HashTable(const HashTable &copy); // copy constructor
+	HashTable & operator=(const HashTable &copy); // copy asssingment 
+};
+
+#endif
